fa2b9b6c74d20134b99612009b3d01e22cb9a0bc
TheWizardN <noahreed1@gmail.com>
2016-04-08 14:04:40 -0400

    Made it

---
+++ collision.lua
@@ -0,0 +1,76 @@
+x = 0
+xV = 0
+y = 0
+yV = 0
+direction = "right"
+collision = {}
+frame = 0
+
+function getDirection()
+  if math.abs(xV) > math.abs(yV) then
+    if xV > 0 then
+      return "left"
+    elseif xV < 0 then
+      return "right"
+    end
+  elseif math.abs(xV) < math.abs(yV) then
+    if yV > 0 then
+      return "front"
+    elseif yV < 0 then
+      return "back"
+    end
+  end
+end
+function addCollision(x1, y1, x2, y2)
+  table.insert(collision, {x1 - bottom:getWidth(), y1 - bottom:getHeight(), x2- bottom:getWidth(), y2 - bottom:getHeight()})
+end
+
+function checkCollision()
+  i = 1
+  while i < #collision + 1 do
+    if y > collision[i][2] and y < collision[i][4] and x > collision[i][1] - 10 and x < collision[i][1] and xV > 0 then
+      x = x - xV
+      xV = 0
+    elseif y > collision[i][2] and y < collision[i][4] and x > collision[i][3] and x < collision[i][3] + 10 and xV < 0 then
+      x = x - xV
+      xV = 0
+    elseif x > collision[i][1] and x < collision[i][3] and y > collision[i][2] - 10 and y < collision[i][2] and yV > 0 then
+      y = y - yV
+      yV = 0
+    elseif x > collision[i][1] and x < collision[i][3] and y > collision[i][4] and y < collision[i][4] + 10 and yV < 0 then
+      y = y - yV
+      yV = 0
+    end
+      i = i + 1
+  end
+end
+function love.update(dt)
+  if love.keyboard.isDown("d") == true then
+    xV = xV - 1
+  end
+  if love.keyboard.isDown("a") == true then
+    xV = xV + 1
+  end
+  if love.keyboard.isDown("w") == true then
+    yV = yV + 1
+  end
+  if love.keyboard.isDown("s") == true then
+    yV = yV - 1
+  end
+  x = x + xV
+  y = y + yV
+  if frame + 0.1 > 4 then
+    frame = 0
+  end
+  frame = frame + (math.abs(xV) / 50 + math.abs(yV) / 50)
+  if xV ~= 0 or yV ~= 0 then
+    checkCollision()
+    direction = getDirection()
+  end
+  if math.abs(xV) < 0.3 and math.abs(yV) < 0.3 then
+    frame = 4
+  end
+  yV = yV * 0.9
+  xV = xV * 0.9
+
+end
---
+++ main.lua
@@ -0,0 +1,78 @@
+function love.load ()
+    char = {}
+    require("collision")
+    love.graphics.getWidth ()
+    love.graphics.getHeight ()
+    love.window.setFullscreen (true)
+    right = love.graphics.newImage("right.png")
+    left = love.graphics.newImage("left.png")
+    front = love.graphics.newImage("front.png")
+    top = love.graphics.newImage ("toplayer.png")
+    bottom = love.graphics.newImage ("backlayer.png")
+    char[1] = love.graphics.newQuad (0, 64, 64, 64, right:getDimensions())
+    char[2] = love.graphics.newQuad (0, 128, 64, 64, right:getDimensions())
+    char[3] = love.graphics.newQuad (0, 192, 64, 64, right:getDimensions())
+    char[4] = love.graphics.newQuad (0, 128, 64, 64, right:getDimensions())
+    char[5] = love.graphics.newQuad (0, 0, 64, 64, right:getDimensions())
+    addCollision(67, 734, 1854, 911)
+    --65, 184, 1852, 345 real cordinates
+    addCollision(67, 94, 1854, 255)
+    --65, 985, 1852, 824 real cordinates
+    logo = love.graphics.newImage ("logo.png")
+    skyline = love.graphics.newImage ("skyline.png")
+    startsprite = love.graphics.newImage ("StartButtons.png")
+    start = love.graphics.newQuad (0, 0, 200, 100, startsprite:getDimensions())
+    back = love.graphics.newQuad (0, 100, 200, 100, startsprite:getDimensions())
+
+    w = love.graphics.getWidth ()
+    h = love.graphics.getHeight ()
+      --Width of starting screen 1366, 768
+    menu = true
+    resolution = math.ceil (w / 1366)
+   function love.draw ()
+       i = 0
+     if menu == true then
+       love.graphics.draw (logo, w / 2 - 500, h / 2 - 600)
+       love.graphics.setBackgroundColor (178, 168, 160)
+       while i < resolution do
+         love.graphics.draw (skyline, i * 1366, h - 868)
+         i = i + 1
+       end
+       if resolution == 0 then
+         love.graphics.draw (skyline, 0, 0 )
+       end
+       love.graphics.draw (startsprite, start, w / 2 - 100, h / 2 - 50)
+       love.graphics.draw (startsprite, back, w / 2 - 100, h / 2 + 160)
+     else
+       love.graphics.draw(bottom, w / 2 + x, h / 2 + y)
+       if direction == "right" then
+         love.graphics.draw(right, char[math.floor(frame) + 1], w / 2 - 32, h / 2 - 64)
+       elseif direction == "left" then
+         love.graphics.draw(left, char[math.floor(frame) + 1], w / 2 - 32, h / 2 - 64)
+       elseif direction == "back" then
+         love.graphics.draw(front, char[math.floor(frame) + 1], w / 2 - 32, h / 2 - 64)
+       elseif direction == "front" then
+         love.graphics.draw(front, char[math.floor(frame) + 1], w / 2 - 32, h / 2 - 64)
+       end
+       love.graphics.draw(top, w / 2 + x, h / 2 + y)
+     end
+
+   function love.mousepressed (x, y, button)
+      if button == 1 then
+        if x > w / 2 - 100 and x < w / 2 + 100 and y > h / 2 - 50 and y < h / 2 + 50  then
+          love.graphics.setBackgroundColor(0, 0, 0)
+          menu = false
+        elseif x > w / 2 - 100 and x < w / 2 + 100 and y > h / 2 + 160 and y < h / 2 + 260 then
+          love.event.quit ()
+        end
+      end
+   end
+
+   function love.keypressed (key)
+     if key == "escape" and menu == false then
+       love.graphics.setBackgroundColor (104, 92, 83)
+       menu = true
+     end
+   end
+ end
+end
